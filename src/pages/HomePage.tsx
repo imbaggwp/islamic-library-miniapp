3123785b1c2c6ee0053aeacc06fcb9936a5dcff3
imbaggwp <asiya30092013@gmail.com>
2026-07-29 10:46:08 +0300

    Первый запуск проекта

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>📖 Библиотека знаний</h1>
+
+      <p>السلام عليكم ورحمة الله وبركاته‌‎ 👋</p>
+
+      <p>Добро пожаловать!</p>
+    </div>
+  );
+}
+
+export default HomePage;
